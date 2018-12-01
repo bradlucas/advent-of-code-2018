f69430ca076b99d869d08bdcff198151f467cbb1
Brad Lucas <brad@beaconhill.com>
2018-12-01 09:12:45 -0500

    project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject advent "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v20.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main advent.core)
