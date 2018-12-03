9b574f4ce14b25f0f4d7fe2505c2a2ab6e9a663d
Brad Lucas <brad@beaconhill.com>
2018-12-03 11:43:35 -0500

    new default message

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,10 +1,8 @@
 (ns advent.core
-  (:require [advent.day01.calibration :as day01])
   (:gen-class))
 
 (defn -main
   [& args]
   (do
     (println "Advent of Code 2018")
-    (println (day01/run-part1))
-    (println (day01/run-part2))))
+    (println "See each day's directory")))
