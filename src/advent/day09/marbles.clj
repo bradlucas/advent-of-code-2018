9db384d290dc2a29e06c4238775ee017d3c2219e
Brad Lucas <brad@beaconhill.com>
2018-12-11 09:38:00 -0500

    day 9 part 2

--- src/advent/day09/marbles.clj
+++ src/advent/day09/marbles.clj
@@ -44,7 +44,7 @@
          player-id 0
          players (vec (repeat num-players 0))]
     ;; let us know that links are still working by printing milestones
-    (if (zero? (mod marble 5000)) (println marble))
+    ;; (if (zero? (mod marble 5000)) (println marble))
     (if (= (mod marble 23) 0)
       ;; special 23 case
       (let [new-idx (counter-clockwise idx 7 circle)
@@ -74,6 +74,110 @@
 )
 
 
+;; ----------------------------------------------------------------------------------------------------
+;; Part2
+;;
+;; The above code takes too long so looking for other ideas
 
-;; 365659  - wrong
+;; (defn part2 []
+;;   ;; 491 players; last marble is worth 71058 * 100 (7,105,800) ppoints
+;;   (apply max (process 491 (* 100 7o1058)))
+;;   )
 
+
+;; ----------------------------------------------------------------------------------------------------
+;; Tried refactoring to remove the loop/recur but this was still too slow
+
+
+;; (defn init-game [num-players]
+;;   {:idx 0
+;;    :marble 0
+;;    :circle [0]
+;;    :player-id 0
+;;    :players (vec (repeat num-players 0))}
+;; )
+;;
+;; (defn process [game]
+;;   (let [{:keys [idx marble circle player-id players]} game]
+;;     (if (= (mod marble 23) 0)
+;;       ;; special 23 case
+;;       (let [new-idx (counter-clockwise idx 7 circle)
+;;             [val new-circle] (remove-at new-idx circle)]
+;;         (assoc game :idx new-idx :marble (inc marble) :circle new-circle :player-id (clockwise player-id 1 players) :players (assoc players player-id (+ (get players player-id) val marble))))
+;;       ;; else
+;;       (let [pos (clockwise idx 1 circle)
+;;             new-circle (insert-after pos marble circle)]
+;;           (assoc game :idx (inc pos) :marble (inc marble) :circle new-circle :player-id (clockwise player-id 1 players) :players players)))
+;;     )
+;; )
+;;
+;; (defn part1 []
+;;   (let [game (-> (iterate process (init-game 491))
+;;                  (nth (dec 71058)))]
+;;     (apply max (:players game)))
+;;   )
+
+
+
+
+;; ----------------------------------------------------------------------------------------------------
+;; Looked to mfikes for a solution
+;; @see https://github.com/mfikes/advent-of-code/blob/master/src/advent_2018/day_09.cljc
+
+
+;; Correct Answer
+;; 2945918550
+
+
+(defn init-game [players]
+  {:players players
+   :player  1
+   :index   1.0
+   :marble  1
+   :circle  (sorted-map 0.0 0 1.0 1)
+   :scores  (zipmap (map inc (range players)) (repeat 0))})
+
+(defn place-index [circle index]
+  (let [[lb ub] (keys (rest (concat (subseq circle >= index) (cycle circle))))]
+    (if (< lb ub)
+      (/ (+ lb ub) 2.0)
+      (inc (ffirst (rseq circle))))))
+
+(defn counter-clockwise-index [circle index n]
+  (-> (keys (concat (rsubseq circle <= index) (cycle (rseq circle))))
+    (nth n)))
+
+(defn advance [game]
+  (let [{:keys [players player index marble circle scores]} game
+        marble (inc marble)
+        player (inc (mod player players))
+        game   (assoc game :marble marble :player player)]
+    (if (= (mod marble 23) 0)
+      (let [remove-index (counter-clockwise-index circle index 7)
+            scores       (update scores player + marble (circle remove-index))
+            circle       (dissoc circle remove-index)
+            index        (first (first (or (subseq circle > remove-index) circle)))]
+        (assoc game :index index :circle circle :scores scores))
+      (let [index  (place-index circle index)
+            circle (assoc circle index marble)]
+        (assoc game :index index :circle circle))
+      )))
+
+(defn solve [num-players last-worth]
+  (let [game (-> (iterate advance (init-game num-players))
+                 (nth (dec last-worth)))]
+    (apply max (vals (:scores game)))))
+
+
+;; Part1 
+;; (time (solve 491 71058)
+;; "Elapsed time: 345922.048125 msecs"
+;; 361466
+
+
+(defn part2 []
+  (time (solve 491 (* 100 71058))))
+
+(comment
+  (part2)   ;;   2945918550
+)
