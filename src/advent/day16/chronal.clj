f3aad4b00c85cf3aa4c47bdbc0ee66346ea2c969
Brad Lucas <brad@beaconhill.com>
2018-12-29 17:59:08 -0500

    day 16 part 2

--- src/advent/day16/chronal.clj
+++ src/advent/day16/chronal.clj
@@ -4,7 +4,6 @@
 
 (def pp clojure.pprint/pprint)
 
-
 (defn parse-state
   "Parse  `Before: [1 2 3 4]` into `[1 2 3 4]`"
   [s]
@@ -42,7 +41,7 @@
 ;; Multiplication:
 
 ;;     mulr (multiply register) stores into register C the result of multiplying register A and register B.
-(defn mult [state a b c]
+(defn mulr [state a b c]
   (assoc state c (* (state a) (state b))))
 
 ;;     muli (multiply immediate) stores into register C the result of multiplying register A and value B.
@@ -114,6 +113,8 @@
 
 
 
+;; ----------------------------------------------------------------------------------------------------
+;; Testing
 
 ;; Before: [3, 2, 1, 1]
 ;; 9 2 1 2
@@ -130,21 +131,91 @@
 ;; Part 1
 ;; How many samples match the operations of three or more opcodes
 
-(defn test-opcode [opcode before inst after]
-  (= after (apply opcode before (rest inst))))
+(defn test-opcode 
+  ([opcode [before inst after]]
+   (test-opcode opcode before inst after))
+  ([opcode before inst after]
+   (= (apply opcode before (rest inst)) after)))
 
-(defn test-opcodes [sample]
+(defn test-opcodes [opcodes sample]
   (let [[before inst after] sample]
     (count (filter #(test-opcode % before inst after) opcodes))))
 
 (defn part1 []
-  (count (filter #(> % 2) (map test-opcodes (load-samples)))))
+  (count (filter #(> % 2) (map #(test-opcodes opcodes %) (load-samples)))))
 
 
 ;; advent.day16.chronal> (part1)
 ;; 646
 
 
+;; ----------------------------------------------------------------------------------------------------
+;; Part 2
+;; Deduce the opcode numbers and then run the sample program (input2.txt)
+
+(defn load-program
+  "Read input2.txt and convert to instructions"
+  []
+  (let [input-file "resources/day16/input2.txt"
+        input-lines (str/split-lines (slurp input-file))]
+    (map #(parse-inst %) input-lines)))
 
 
+(defn run-program [instructions opcode-map]
+  (reduce (fn [state [op a b c]]
+            ((opcode-map op) state a b c))
+          [0 0 0 0]
+          instructions))
 
+
+(defn op-set [acc ops [before [opcode & args] after]]
+  (reduce
+    (fn [m op]
+      (if (= (apply op before args) after)
+        (update m opcode (fnil conj #{}) op)
+        m))
+    acc
+    ops))
+
+(defn num->ops [samples ops]
+  (loop [samples samples
+         acc {}]
+    (if (empty? samples)
+      acc
+      (recur (rest samples) (op-set acc ops (first samples))))))
+
+
+(defn find-singles [m]
+  (filter (fn [[num ops]] (= (count ops) 1)) m))
+
+
+(defn deduce
+  "num->ops returns map with keys which are operand number and value a list of potential instructions.
+Figure out which num goes to which function. Start by filtering 'known' values. Those with single match
+"
+  [samples opcodes]
+  (loop [acc {}
+         unassigned (into #{} opcodes)]
+    ;; (pp acc)
+    ;; (pp unassigned)
+    (if (empty? unassigned)
+      acc
+      (let [ops-map (num->ops samples (vec unassigned))
+            singles (find-singles ops-map)
+            single (first singles)
+            opcode (first single)
+            fnc (first (second single))]
+        (recur (assoc acc opcode fnc) (disj unassigned fnc))
+        ))))
+
+
+(defn part2 []
+  (let [op-map (deduce (load-samples) opcodes)
+        program (load-program)]
+    (let [[r0 r1 r2 r3] (run-program program op-map)]
+      r0)))
+
+
+(comment
+  (part2)    ;; 681
+)
