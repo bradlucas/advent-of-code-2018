248041249a5a8f43c31d7ae87e3026c7de78be68
Brad Lucas <brad@beaconhill.com>
2020-01-03 16:27:22 -0500

    day 22 part 2

--- src/advent/day22/modemaze.clj
+++ src/advent/day22/modemaze.clj
@@ -1,5 +1,6 @@
 (ns advent.day22.modemaze
-  (:require [clojure.set :as set]))
+  (:require [clojure.pprint :as pp]
+            [clojure.set :as set]))
 
 ;; The following was helped by https://github.com/borkdude/advent-of-cljc/blob/master/src/aoc/y2018/d22/iamdrowsy.cljc#L18
 
@@ -43,3 +44,94 @@
 (defn part1 []
   (reduce + (map-types (build-map depth target target))))
 
+
+
+
+;; ----------------------------------------------------------------------------------------------------
+;; Part 2
+;; Again see https://github.com/borkdude/advent-of-cljc/blob/master/src/aoc/y2018/d22/iamdrowsy.cljc#L18 
+
+;; Start at 0,0 with a torch and climbing gear
+
+;; Tools => climbing, torch
+
+;; 0 == rocky =>  climbing, or torch;    you can not use neither (you have to use climbing or torch)
+;; 1 == wet   =>  climbing, or neither;  you can not  use the torch
+;; 2 == narrow => torch or neither;      you can not use climbing
+
+;; 
+;; Movements are up, down, left or right (no diagonal)
+
+
+;; Can only move to a new regtion is you have the proper tool
+;; Moving to an adjacent region takes 1 minute
+;; Switching tools takes 7 minutes
+
+;; Target is always rocky
+
+;; Diagram
+;; rocky => .
+;; wet => =
+;; narrow => |
+
+(defn build-type-map
+  "Convert the erosion map to one with type values.
+
+[[17 730] 17033]
+
+=>
+
+[[17 730] 2]
+"
+  [m]
+  (map (fn [[a b]] [a (mod b 3)]) m))
+
+
+(defn all-valid
+  " Build set of valid position tool possiblities
+    "
+  [type-map]
+    (reduce (fn [all [[x y] t]]
+                 (case t 0 (conj all [x y :torch][x y :climbing])
+                         1 (conj all [x y :none] [x y :climbing])
+                         2 (conj all [x y :none] [x y :torch])))
+               #{}
+               type-map))
+
+(defn switch-tool
+  "For a given tool return the other choices"
+  [[x y t]]
+  (map (fn [t] [x y t]) (disj #{:none :torch :climbing} t)))
+
+(defn move-single
+  "Return the next positions"
+  [[x y t]]
+  #{[(inc x) y t] [(dec x) y t] [x (inc y) t] [x (dec y) t]})
+
+(defn move [current]
+  (reduce set/union (map move-single current)))
+
+(defn solve-2 [depth target]
+  (let [t (conj target :torch)
+        ;; (* 7 %) is an heuristic to generate a cave big enough we would never leave it
+        type-map (build-type-map (build-map depth target (map #(* 7 %) target)))
+        all-valid (all-valid type-map)]
+    (loop [time 1
+           not-visited all-valid
+           reachable {0 [[0 0 :torch]]}]
+      (let [next (set/intersection (into (move (reachable (dec time) []))
+                                         (mapcat switch-tool (reachable (- time 7) [])))
+                                   not-visited)]
+        (if (next t)      ;; at target
+          time
+          (recur (inc time) (set/difference not-visited next) (assoc reachable time next)))))))
+
+
+(defn part2 []
+  (solve-2 depth target))
+
+
+(comment
+  (part2)
+  ;; => 1010
+  )
